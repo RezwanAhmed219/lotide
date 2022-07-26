2053d8a3be81b119f1f95c975824fa1e9bd6f8cb
Rezwan Ahmed <rezwan.ahmed.258@gmail.com>
2022-07-26 02:16:14 +0000

    assertEqual completed

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: "${actual}" === "${expected}"`)
+  } else if (actual !== expected)
+    console.log(`🔴🔴🔴 Assertion Failed: "${actual}" !== "${expected}"`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+console.log("")
+
+assertEqual(1, 1);
+
+
