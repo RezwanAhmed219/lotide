15e72e64de6e89ab8e199f3bfe9ade6f2d6880d7
Rezwan Ahmed <rezwan.ahmed.258@gmail.com>
2022-07-28 04:21:15 +0000

    incomplete assertObjectWqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,29 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {  
+    return false; }
+  for (let key in object1) {
+    if(Array.isArray(object1[key] === Array.isArray(object2[key]))) {
+      return false;
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+    return true;
+  };
+
+// CODES HERE
+
+  const assertObjectsEqual = function(actual, expected) {
+    const inspect = require('util').inspect;
+    if (eqObjects(actual, expected)) {
+      console.log(`✅✅✅ ${inspect(actual)} === ${inspect(expected)}`) 
+    } else {
+      console.log(`🔴🔴🔴 ${inspect(actual)} !== ${inspect(expected)}`)
+    }
+
+  };
+
+// TEST CODE
+
