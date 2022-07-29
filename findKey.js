49a2893047b6c51d43eef340a6e1824fef9c515b
Rezwan Ahmed <rezwan.ahmed.258@gmail.com>
2022-07-29 00:50:13 +0000

    updated findkeys

---
+++ findKey.js
@@ -0,0 +1,32 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: "${actual}" === "${expected}"`)
+  } else if (actual !== expected)
+    console.log(`🔴🔴🔴 Assertion Failed: "${actual}" !== "${expected}"`);
+};
+
+// findKey Code
+
+const findKey = function(array, callback) {
+  for (const key in array) {
+    if (callback(array[key])) {
+      return key;
+    }
+  }
+  return undefined 
+};
+
+
+
+
+assertEqual((findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)), "noma");
+
